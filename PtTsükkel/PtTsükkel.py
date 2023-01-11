7f6ef1e86eb2d519a0f7b1cce8428830e9a97e7b
opilane <stanislav.tsugunov@gmail.com>
2023-01-11 16:39:00 +0200

    Add project files.

---
+++ PtTsükkel/PtTsükkel.py
@@ -0,0 +1,63 @@
+from math import * 
+from random import * 
+#Mõtle ise välja ülesanne, mis on vaja lahendada while tingimusega/while True/for kasutades. Lahenda nii while kui ka for abil.
+   
+for i in range(1,5):
+    x=str("*"*i).center(18," ")
+    print(x, end="")
+    print()
+for i in range(1,7):
+    x=str("*"*(1+2)).center(18," ")
+    print(x, end="")
+    print()
+for i in range(1,10):
+    x=str("*"*(i+4)).center(18," ")
+    print(x, end="")
+    print()
+   #
+#num = 5
+#while num <= 50:
+#    print("number",num)
+#    num += 1
+
+#x=5
+#while True: 
+#    if x%5==10:
+#        print(x, end=" ")
+#    x+=1
+#    if x==50: break
+#print()
+
+#2(6)
+#n=0
+#print("kolmnurga")
+#for e in range (11, 0,-1):
+#    n = n + 1
+#    for f in range (1, n+1):
+#        print("*", end = "")
+#    print()
+#print("")
+
+#ülesanne 1(6)
+#for x in range(S):
+#    print("******")
+
+#Ülesanne 9 Pisike korrutustabel
+
+n=int(input("Sisestage number n: " ))
+for i in range(1,11):
+    print(f' {5} * {i} = {n*i} ')
+print()
+
+#=5
+#while True:
+#    num=("Sisestage number: ")
+#    if x%5==11:
+#        print("x, end=")
+#    if
+#else:
+print()
+
+
+
+
